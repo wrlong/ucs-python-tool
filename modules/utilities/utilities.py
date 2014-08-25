48e7a344ab0289e82d0a32c7bd647c101324c0cf
Tory Long <wrlong@cisco.com>
2014-08-24 21:48:48 -0400

    More refactoring.

--- modules/utilities/utilities.py
+++ modules/utilities/utilities.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+import time
+import datetime
+import getpass
 
 def get_remove_dn_string(Dn, class_string):
     return 'handle.RemoveManagedObject(handle.GetManagedObject(None, ' +\
@@ -11,3 +14,19 @@ def pad_zeros(int_as_string, num_digits):
 
 def int_to_string(int_to_convert):
     return str(int_to_convert)
+
+def timestamp_str():
+    return datetime.datetime.fromtimestamp(current_time()).strftime('%Y-%m-%d-%H-%M-%S')
+
+def current_time():
+    return time.time()
+
+def invoking_user():
+    return getpass.getuser()
+
+def unique_description():
+    return "Created via Python by " + invoking_user() + \
+           " - " + timestamp_str()
+
+def add_to_remove_script(fh, Dn, class_string):
+    fh.write(get_remove_dn_string(Dn, class_string))
